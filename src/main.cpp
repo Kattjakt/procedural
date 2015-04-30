4f4e233e1426b6ffbddf69f2b8a3e85c910bf52e
Leo Sandström <leosand97@gmail.com>
2015-04-30 18:40:07 +0200

    add Windows-specific messagebox for exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include "procedural.h"
 
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
 int main(int argc, char* argv[]) {
-    Procedural *p = new Procedural();
+    Procedural procedural;
+
 	try {
-		p->run();
+		procedural.go();
 	} catch(Ogre::Exception &e) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
 		std::cerr << "An exception has occurred: " << 
 		  e.getFullDescription().c_str() << std::endl;
 		std::cin.ignore();
+#endif
 	}
-
     return EXIT_SUCCESS;
 }
