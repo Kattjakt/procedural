89c3ced2c4f6d0747911939bf49f07c6f48931de
Leo Sandström <leosand97@gmail.com>
2015-05-02 22:26:21 +0200

    can now generate a 6-faced cube with shitty coordinates

--- src/planet.cpp
+++ src/planet.cpp
@@ -6,31 +6,62 @@ Planet::Planet(float radius) {
 
 void Planet::generate() {
 	cube = new Ogre::ManualObject("Planet");
-	cube->begin("DebugGreenWireframe", Ogre::RenderOperation::OT_TRIANGLE_LIST);
+	cube->begin("DebugGreenWireframe", Ogre::RenderOperation::OT_TRIANGLE_LIST); 
 
 #define MAX 20
 
-	for (int x = 0; x < MAX; x++) {
-		for (int y = 0; y < MAX; y++) {
-			cube->position(x, y, /* static_cast <float> (rand()) / static_cast <float> (RAND_MAX) * 5 */ 0.0f);
+	float c = 0;
+
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(a, b, c); //  some side
+		}
+	}
+	
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(a, c, b); // bottom
 		}
 	}
 
-	for (int x = 0; x < MAX - 1; x++) {
-		for (int y = 0; y < MAX - 1; y++) {
-			cube->triangle(
-				(y * MAX) + x + 1, 
-				((y + 1) * MAX) + x,
-				((y + 1) * MAX) + x + 1
-			);
-			cube->triangle(
-				((y + 1) * MAX) + x,
-				(y * MAX) + x + 1, 
-				(y * MAX) + x
-			);
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(c, a, b); //  some side
 		}
 	}
-		
+
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(a, c + MAX - 1, b); // top
+		}
+	}
+
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(c + MAX - 1, a, b); //  some side
+		}
+	}
+
+	for (int a = 0; a < MAX; a++) {
+		for (int b = 0; b < MAX; b++) {
+			cube->position(a, b, c + MAX - 1); //  some side
+		}
+	}
+
+	for (int face = 0; face <= 5; face++) {
+		for (int x = 0; x < MAX - 1; x++) {
+			for (int y = 0; y < MAX - 1; y++) {
+				cube->quad( 
+					//   vertex offset           local offset
+					(pow(MAX, 2) * face) + ((y + 1) * MAX) + x + 1,
+					(pow(MAX, 2) * face) + (y* MAX) + x + 1,
+					(pow(MAX, 2) * face) + (y* MAX) + x, 
+					(pow(MAX, 2) * face) + ((y + 1) * MAX) + x
+				);
+			}
+		}
+	}
+
 	cube->end();
 }
 
